1643c196f5132bb17fd7dbcc5c28455a7a298be4
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2025-02-02 00:54:17 +0330

    ShowTasks added

--- src/components/Tasks/NewTask.tsx
+++ src/components/Tasks/NewTask.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 
-export const NewTask = (props: { addTask: (taskInput: string) => void }) => {
-  const [NewTaskInput, setNewTaskInput] = useState("");
+export const NewTask = (props: { addTask: (taskInputTitle: string, taskInputDescription: string) => void }) => {
+  const [NewTaskInputTitle, setNewTaskInputTitle] = useState("");
+  const [NewTaskInputDescription, setNewTaskInputDescription] = useState("");
   function handleSubmit() {
-    props.addTask(NewTaskInput);
-    setNewTaskInput("");
+    props.addTask(NewTaskInputTitle, NewTaskInputDescription);
+    setNewTaskInputTitle("");
+    setNewTaskInputDescription("");
   }
   return (
-    <div className="p-3 mt-5 w-full flex items-center gap-2 rounded-2xl border-2 border-zinc-400 dark:border-zinc-600 border-dashed">
-      <button onClick={handleSubmit}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 border-2 rounded-full opacity-50 hover:bg-zinc-700 hover:border-zinc-700 hover:stroke-zinc-50">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-        </svg>
-      </button>
-      <input value={NewTaskInput} onChange={(e) => setNewTaskInput(e.target.value)} type="text" placeholder="تسک جدید..." className="w-full text-lg bg-transparent outline-none" />
+    <div className="p-3 mt-5 w-full  rounded-2xl border-2 border-zinc-400 dark:border-zinc-600 border-dashed">
+      <div className="flex items-center gap-2">
+        <button onClick={handleSubmit}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 border-2 rounded-full opacity-50 hover:bg-zinc-700 hover:border-zinc-700 hover:stroke-zinc-50">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </button>
+        <input value={NewTaskInputTitle} onChange={(e) => setNewTaskInputTitle(e.target.value)} type="text" placeholder="تسک جدید..." className="w-full text-lg bg-transparent outline-none" />
+      </div>
+      <input value={NewTaskInputDescription} onChange={(e) => setNewTaskInputDescription(e.target.value)} type="text" placeholder="توضیحات" className="w-full mt-2 mr-8 text-sm bg-transparent outline-none" />
     </div>
   );
 };
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
-import { NewTask } from './NewTask'
+import React, { useState } from "react";
+import { NewTask } from "./NewTask";
+import { ITask } from "./ITask";
+import { ShowTasks } from "./ShowTasks";
 export const Tasks = () => {
-  const [tasks, setTasks] = useState<string[]>([]);
-  function addTask(taskInput: string) {
-    setTasks([...tasks, taskInput]);
-    
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  function addTask(taskInputTitle: string, taskInputDescription: string) {
+    setTasks([...tasks, { id: tasks.length + 1, title: taskInputTitle, description: taskInputDescription }]);
   }
   return (
-    <div className='w-full mt-20'>
-      <div className='text-2xl font-bold'>تسک های امروز</div>
-      <div className='opacity-50'>3 تسک رو باید انجام بدی</div>
-      <NewTask addTask={addTask}/>
-      {tasks.map((task, index) => (
-        <div key={index}>{task}</div>
-      ))}
+    <div className="w-full mt-20">
+      <div className="text-2xl font-bold">تسک های امروز</div>
+      <div className="opacity-50">3 تسک رو باید انجام بدی</div>
+      <NewTask addTask={addTask} />
+      <ShowTasks tasks={tasks} />
     </div>
-  )
-}
+  );
+};
