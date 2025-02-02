1c39df9a42a1e7fb630347aeab2877f13680804d
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2025-02-02 14:15:05 +0330

    done task added, task delete edited

---
+++ src/components/Tasks/ShowDoneTasks.tsx
@@ -0,0 +1,35 @@
+import { ITask } from "./ITask";
+
+interface ShowDoneTasksProps {
+  doneTasks: ITask[];
+  taskDelete: (taskId: number, doneOrTasks: string) => void;
+  doTask: (task: ITask, isDone: boolean) => void;
+}
+export const ShowDoneTasks = ({ doneTasks, taskDelete, doTask }: ShowDoneTasksProps) => {
+  return (
+    <div>
+      {doneTasks.map((task, index) => (
+        <div key={index} className="p-4 w-full mt-4 flex items-center justify-between rounded-2xl border-[1px] border-zinc-400 dark:border-zinc-600">
+          <div className="flex items-center w-full gap-4">
+            <div className="w-1 h-16 -mr-4 bg-zinc-500 rounded-l-lg rounded-bl-lg"></div>
+            <input className="size-6 -mr-1 rounded-2xl" type="checkbox" name="taskDone" id={index.toString()} checked={doneTasks.includes(task)} onChange={(e) => doTask(task, e.target.checked)} />
+            <div className="w-full">
+              <div className="text-lg font-bold" key={index}>
+                <input className="w-full outline-none" value={task.title} type="text" readOnly />
+              </div>
+              <div className="text-sm opacity-75" key={index}>
+                <input className="w-full outline-none" value={task.description} type="text" readOnly />
+              </div>
+            </div>
+          </div>
+          {/* delete icon */}
+          <div className="group p-1 hover:bg-current rounded-md">
+            <svg onClick={() => taskDelete(task.id, "done")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 group-hover:stroke-zinc-50 dark:group-hover:stroke-zinc-700 transition-all">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,24 +1,42 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { NewTask } from "./NewTask";
 import { ITask } from "./ITask";
 import { ShowTasks } from "./ShowTasks";
+import { ShowDoneTasks } from "./ShowDoneTasks";
 export const Tasks = () => {
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const [doneTasks, setdoneTasks] = useState<ITask[]>([]);
   function addTask(taskInputTitle: string, taskInputDescription: string) {
-    setTasks([...tasks, { id: tasks.length + 1, title: taskInputTitle, description: taskInputDescription }]);
+    setTasks([...tasks, { id: Date.now(), title: taskInputTitle, description: taskInputDescription }]);
   }
-  function editTask (taskId: number, taskTitle: string, taskDescription: string) {
+  function editTask(taskId: number, taskTitle: string, taskDescription: string) {
     setTasks(tasks.map((task) => (task.id === taskId ? { ...task, title: taskTitle, description: taskDescription } : task)));
   }
-  function taskDelete (taskId: number) {
-    setTasks(tasks.filter(task => task.id !== taskId));
+  function taskDelete(taskId: number, doneOrTasks: string) {
+    if (doneOrTasks === "done") {
+      setdoneTasks(doneTasks.filter((task) => task.id !== taskId));
+    } else {
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    }
+  }
+  function doTask(toBeDonetask: ITask, isDone: boolean) {
+    if (isDone) {
+      setTasks(tasks.filter((task) => task.id !== toBeDonetask.id));
+      setdoneTasks([...doneTasks, toBeDonetask]);
+    } else {
+      setTasks([...tasks, toBeDonetask]);
+      setdoneTasks(doneTasks.filter((task) => task.id !== toBeDonetask.id));
+    }
   }
   return (
     <div className="w-full mt-20">
       <div className="text-2xl font-bold">تسک های امروز</div>
       <div className="opacity-50">3 تسک رو باید انجام بدی</div>
       <NewTask addTask={addTask} />
-      <ShowTasks tasks={tasks} taskDelete={taskDelete} editTask={editTask}/>
+      <ShowTasks tasks={tasks} taskDelete={taskDelete} editTask={editTask} doTask={doTask} />
+      <div className="text-2xl font-bold">تسک های انجام شده</div>
+      <div className="opacity-50">3 تسک رو انجام دادی</div>
+      <ShowDoneTasks doneTasks={doneTasks} taskDelete={taskDelete} doTask={doTask} />
     </div>
   );
 };
