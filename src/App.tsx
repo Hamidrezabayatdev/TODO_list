f0af34e85419246b031cada6699c007ce56cf4e6
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2025-02-05 22:48:04 +0330

    fixed insteade of absolute

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Tasks } from "./components/Tasks/Tasks";
 function App() {
   return (
     <div className="relative container mx-auto py-5 px-4 md:px-0 h-screen">
-      <div className="absolute fixed top-0 bottom-0 hidden md:block my-auto w-80 h-[90vh]">
+      <div className="fixed top-0 bottom-0 hidden md:block my-auto w-80 h-[90vh]">
         <Menu />
       </div>
       <div className="md:pr-92 lg:pr-[416px] xl:pr-[448px] 2xl:pr-[512px]">
